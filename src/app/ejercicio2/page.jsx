4193e5e8827654b3146d0334de7632ad5574152b
Nelson Camargo <cmargosb@gmail.com>
2024-02-29 19:51:17 +0100

    card no terminado

--- src/app/ejercicio2/page.jsx
+++ src/app/ejercicio2/page.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+'use client'
 
-const Ejercicio2 = () => {
-  return (
-    <div></div>
-  )
+import React, { useEffect, useState } from 'react';
+
+const DigitalClock = () => {
+
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTime(new Date())
+
+        }, 1000)
+        console.log(time)
+
+        return () => clearInterval(timer)
+
+    }, [])
+
+
+
+    return (
+        <div className='flex justify-center bg-white items-center h-screen'>
+            <div className='flex justify-center text-7xl flex-col items-center border-8 border-green-400 p-5 rounded-xl bg-black'>
+                <h2 className='text-green-400 font-bold'>{time.toLocaleTimeString()}</h2>
+            </div>
+        </div>
+    )
 }
 
-export default Ejercicio2
+export default DigitalClock
